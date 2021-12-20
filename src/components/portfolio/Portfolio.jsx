5d9eb3a385fea7b726f834751f114c37f4e41680
clayhindman01 <clayhindman01@gmail.com>
2021-12-19 22:21:06 -0500

    Began Adding more responsiveness for other screens.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,23 +4,16 @@ import ChevronRight from "@material-ui/icons/ChevronRight"
 
 export default function Portfolio() {
     return (
-        <div className='portfolio' id="portfolio">
-            <div className="slider">
-                <div className="container">
-                    <div className="background">
-                        <h1>Portfolio</h1>
-                    </div>
-                    <ChevronLeft className="chevronLeft arrow" />
-                    <div className="item">
-                        <div className="left">
-                            
-                        </div>
-                        <div className="right">
-                            
-                        </div>
-                    </div>
-                    <ChevronRight className='chevronRight arrow'/>
+        <div className="portfolio" id="portfolio">
+            <div className="left">
+                <div className="background">
+                    <h1>Portfolio</h1>
+                    
                 </div>
+            </div>
+
+            <div className="right">
+
             </div>
         </div>
     )
