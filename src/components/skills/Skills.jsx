152dd370a3a39896011d5b42f4869a9d7c2c0627
clayhindman01 <clayhindman01@gmail.com>
2022-01-02 19:17:14 -0500

    added new icons for the skills page

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -12,33 +12,56 @@ export default function Skills() {
                     </a>
                 </div>
 
+                <div className="jsIcon item">
+                    <a href="https://www.javascript.com/">
+                        <img src="assets/kisspng-javascript-computer-icons-scalable-vector-graphics-list-of-javascript-enhancements-fandom-developers-5d145895da3992.0726245815616144858939.png" alt="javascript" />
+                    </a>
+                </div>
+
                 <div className="reactIcon item">
-                    
-                    <img src="assets/pngaaa.com-2507930.png" alt="react" />
+                    <a href="https://reactjs.org/">
+                        <img src="assets/pngaaa.com-2507930.png" alt="react" />
+                    </a>
                 </div>
 
                 <div className="nodeIcon item">
-                    <img src="assets/pngaaa.com-4547668.png" alt="node" />
+                    <a href="https://nodejs.org/en/">
+                        <img src="assets/pngaaa.com-4547668.png" alt="node" />
+                    </a>
                 </div>
 
                 <div className="javaIcon item">
-                    <img src="assets/kisspng-logo-java-runtime-environment-programming-language-java-util-concurrentmodificationexception-Ömer-5b6766ab5dba25.7100170215335031473839.png" alt="java" />
+                    <a href="https://www.java.com/en/">
+                        <img src="assets/kisspng-logo-java-runtime-environment-programming-language-java-util-concurrentmodificationexception-Ömer-5b6766ab5dba25.7100170215335031473839.png" alt="java" />
+                    </a>
                 </div>
 
                 <div className="pythonIcon item">
-                    <img src="assets/icons8-python-240.png" alt="python" />
+                    <a href="https://www.python.org/">
+                        <img src="assets/icons8-python-240.png" alt="python" />
+                    </a>
+                </div>
+
+                <div className="dartIcon item">
+                    <a href="https://dart.dev/">
+                        <img src="assets/Dart-logo.png" alt="dart" />
+                    </a>
                 </div>
 
                 <div className="flutterIcon item">
-                    <img src="assets/file_type_flutter_icon_130599.png" alt="flutter" />
+                    <a href="https://flutter.dev/">
+                        <img src="assets/file_type_flutter_icon_130599.png" alt="flutter" />
+                    </a>
                 </div>
 
                 <div className="sqlIcon item">
-                    <img src="assets/kisspng-postgresql-pgadmin-computer-icons-database-depende-elephants-5ad386c474c6b5.5149032415238120364783.png" alt="sql" />
+                    <img src="assets/pngwing.com.png" alt="sql" />
                 </div>
 
                 <div className="firebaseIcon item">
-                    <img src="assets/kisspng-firebase-cloud-messaging-computer-icons-google-clo-github-5ad5d3ce4fc460.4730334415239628303267.png" alt="firebase" />
+                    <a href="https://firebase.google.com/">
+                        <img src="assets/kisspng-firebase-cloud-messaging-computer-icons-google-clo-github-5ad5d3ce4fc460.4730334415239628303267.png" alt="firebase" />
+                    </a>
                 </div>
             </div>
         </div>
