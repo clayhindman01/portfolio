92f8762f9bde85444680407039a5062ee7efe044
clayhindman01 <clayhindman01@gmail.com>
2022-01-11 17:47:37 -0500

    Added being able to send emails from the contact page.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,27 +1,36 @@
-import "./Contact.scss"
-
-function handleClick() {
-    console.log("This is working");
-}
+import "./Contact.scss";
+import emailjs from 'emailjs-com';
 
 export default function Contact() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm("service_ab3x0bg", "template_jsg9o46", e.target, "user_Q4YWJFQfUGRN7ZrNUUSm3")
+        .then((result) => {
+            window.location.reload()
+            console.log("email sent.")   
+        }, (error) => {
+            console.log(error.text);
+        });
+    }
+
     return (
         <div className="contact" id="contact">
             <div className="background">
-                <div className="left">
+                <div className="leftContact">
                     <h3>Have a Question?</h3>
                     <h1>Contact Me</h1>
                 </div>
-                <div className="right">
-                    <div className="contactForm">
-                        <input className="fname" id="fname" name="fname" placeholder="First Name" required></input>
-                        <input className="lname" id="lname" name="lname" placeholder="Last Name" required></input>
+                <div className="rightContact">
+                    <form className="contactForm" onSubmit={sendEmail}>
+                        <input className="from_name" id="from_name" name="from_name" placeholder="Name" required></input>
+                        <input className="from_email" id="from_email" name="from_email" placeholder="Email Address" required></input>
                         <input className="subject" id="subject" name="subject" placeholder="Subject" required></input>
                         <textarea className="message" id="message" name="message" placeholder="Message" height="20px" required></textarea>
-                        <input type="submit" value="Send Message" onClick={handleClick} className="submit" name="submit" id="submit"></input>
-                    </div>
+                        <input type="submit" value="Send Message" className="submit" name="submit" id="submit"></input>
+                    </form>
                 </div>
-                
             </div>
         </div>
     )
