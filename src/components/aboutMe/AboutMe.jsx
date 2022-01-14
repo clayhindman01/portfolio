73816ea709b56b37a24044197e8ff88407463ecf
clayhindman01 <clayhindman01@gmail.com>
2022-01-14 15:27:27 -0500

    added resume

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -17,6 +17,11 @@ export default function AboutMe() {
 
                     In my free time, I enjoy fly fishing and skeet shooting. I also love to go rock climbing and hiking whenever I have the time to do it. I also enjoy cooking and trying different foods.
                     </p>
+
+                    <h2>Resume</h2>
+                    <a href="https://docs.google.com/document/d/1tHUFJGooN1JyY224bEZbBDZVCMM1GawG/edit?usp=sharing&ouid=110812433163043489675&rtpof=true&sd=true">
+                        <img src="assets/writing.png" alt="resume"></img>
+                    </a>
                 </div>
             </div>
         </div>
